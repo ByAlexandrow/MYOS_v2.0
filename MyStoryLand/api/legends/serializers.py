5ddd0ebf4c9a2f74c93ef5b0edbbdb6565784967
ByAlexandrow <egor.aleksx@yandex.ru>
2025-05-18 11:49:02 +0300

    Added audio field

--- MyStoryLand/api/legends/serializers.py
+++ MyStoryLand/api/legends/serializers.py
@@ -8,6 +8,6 @@ class LegendSerializer(serializers.ModelSerializer):
         model = Legend
         fields = [
             'id', 'title_name', 'title_img', 'title_description',
-            'tag_color', 'author', 'created_at', 'is_published'
+            'audio', 'tag_color', 'author', 'created_at', 'is_published'
         ]
         read_only_fields = ['id', 'created_at']
